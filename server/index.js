e44eb7f6730a495b814270e06f1d00d834fca486
Norka Avalos <norka.avalos11@gmail.com>
2019-04-13 16:27:35 -0400

    base working

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8090;
+
+// you'll of course want static middleware so your browser can request things like your 'bundle.js'
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Any routes or other various middlewares should go here!
+app.use('/api', require('./api'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// Make sure this is right at the end of your server logic!
+// The only thing after this might be a piece of middleware to serve up 500 errors for server problems
+// (However, if you have middleware to serve up 404s, that go would before this as well)
+app.get('*', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+app.listen(PORT, () => {
+  console.log(`Making it happening on port ${PORT}`);
+});
